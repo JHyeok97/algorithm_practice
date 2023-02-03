008d9f3789914f8a3c8b9fafcc36162ba363e57f
JHyeok97 <95965218+JHyeok97@users.noreply.github.com>
2023-02-03 15:19:01 +0900

    [Bronze I] Title: ROT13, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11655. ROT13/ROT13.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	string str;
+
+	getline(cin, str);
+
+	for (int i = 0; i < str.length(); i++) {
+		if (str[i] >= 'A' && str[i] <= 'Z') {
+			if (str[i] - 'A' < 13) {
+				str[i] += 13;
+			}
+			else str[i] -= 13;
+		}
+		else if (str[i] >= 'a' && str[i] <= 'z') {
+			if (str[i] - 'a' < 13) {
+				str[i] += 13;
+			}
+			else str[i] -= 13;
+		}
+	}
+	cout << str;
+}
