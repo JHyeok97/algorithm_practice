91d8d8023ff9b075b422075953f94390df79b7b3
JHyeok97 <95965218+JHyeok97@users.noreply.github.com>
2023-06-15 18:27:46 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,24 +1,23 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 int main()
 {
     int n;
-
     cin >> n;
-    vector<int> v(n + 1);
+    vector<int> d(n + 1);
 
-    v[1] = 0;
+    d[1] = 0;
 
     for (int i = 2; i <= n; i++)
     {
-        v[i] = v[i - 1] + 1;
-        if ((i % 3) == 0)
-            v[i] = min(v[i], v[i / 3] + 1);
-        if ((i % 2) == 0)
-            v[i] = min(v[i], v[i / 2] + 1);
+        d[i] = d[i - 1] + 1;
+        if (i % 2 == 0)
+            d[i] = min(d[i], d[i / 2] + 1);
+        if (i % 3 == 0)
+            d[i] = min(d[i], d[i / 3] + 1);
     }
-    cout << v[n];
 
-    return 0;
+    cout << d[n];
 }
