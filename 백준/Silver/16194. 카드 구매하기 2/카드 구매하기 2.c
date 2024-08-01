f94856615258a2b6b27371045b506d6d8be2daaa
JHyeok97 <95965218+JHyeok97@users.noreply.github.com>
2024-08-01 14:20:06 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+#define Min(x, y) x < y ? x : y
+
+int main(void)
+{
+    int N;
+    int P[10001] = {0};
+    int d[10001] = {0};
+
+    scanf("%d", &N);
+
+    for (int i = 1; i <= N; i++)
+    {
+        scanf("%d", &P[i]);
+    }
+
+    d[1] = P[1];
+
+    for (int i = 2; i <= N; i++)
+    {
+        d[i] = P[i];
+
+        for (int j = 1; j <= i / 2; j++)
+        {
+            d[i] = Min(d[i], d[i - j] + d[j]);
+        }
+    }
+
+    printf("%d\n", d[N]);
+
+    return 0;
+}
